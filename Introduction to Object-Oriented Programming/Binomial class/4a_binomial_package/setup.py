d856e5b9f8185fd03df8f9783ce3d14ea2e2be2f
Gaurav Agrawal <gauravag.1005@gmail.com>
2021-09-30 01:17:06 +0530

    #7 Exercise: Binomial class

---
+++ Introduction to Object-Oriented Programming/Binomial class/4a_binomial_package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      author='Gaurav Agrawal',
+      author_email='gauravag.1005@gmail.com',
+      packages=['distributions'],
+      zip_safe=False)
